dfe67fded7aa22fc22cfeffd2edc2790067e8c93
xavki <xavki@moi.me>
2021-04-08 08:13:37 +0200

    update 124 : update module xavki_elasticsearch

--- 124-creation-module-insert-elasticsearch/xavki_elasticsearch.py
+++ 124-creation-module-insert-elasticsearch/xavki_elasticsearch.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python
-#https://docs.ansible.com/ansible/latest/dev_guide/testing/sanity/future-import-boilerplate.html
-#https://docs.ansible.com/ansible/latest/dev_guide/testing/sanity/metaclass-boilerplate.html
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
-
 
 DOCUMENTATION = '''
 module: Xavki_Elasticsearch
 short_description: Add and Get values from elasticsearch cluster
 description:
-  - Allows to insert data into an elasticsearch cluster. For example to store facts or whatever
+  - Allows to insert data into an elasticsearch cluster. For example to store facts or whatever        
   - This module create an index and add document one by one (not by bulk injection)
   - Uses the elasticsearch python librarya - See https://github.com/elastic/elasticsearch-py
 requirements:
   - python3-elasticsearch
-  - requests
+  - python3-requests
 author:
   - Xavier Pestel (alias xavki)
 options:
     state:
         description:
-          - The state helps you to select different actions. Three states are available. 
+          - The state helps you to select different actions. Three states are available.
             Absent to remove the index.
             Present to create an index and store a document.
             Acquire to get all documents in an specific index.
@@ -52,12 +49,8 @@ options:
         type: dict
 '''
 
-
-EXAMPLES = '''
-
-'''
-
 from ansible.module_utils._text import to_text
+from ansible.module_utils.basic import AnsibleModule
 try:
     import elasticsearch as elastic
     import json
@@ -66,18 +59,15 @@ try:
 except ImportError:
     python_elasticsearch_installed = False
 
-from ansible.module_utils.basic import AnsibleModule
-
 NOT_SET = None
 
 def execute(module):
     state = module.params.get('state')
-
     if state == 'present' :
         add_document(module)
     elif state == 'absent':
         delete_document(module)
-    elif state == 'checked':
+    elif state == 'acquire':
         get_documents(module)
     else:
         module.exit_json(msg="Unsupported state: %s" % (state, ))
@@ -86,24 +76,27 @@ def get_elasticsearch_connect(module):
     scheme = module.params.get('scheme')
     host = module.params.get('host')
     port = module.params.get('port')
-
     connect = "{}://{}:{}".format(module.params.get('scheme'), module.params.get('host'), module.params.get('port'))
     return elastic.Elasticsearch(connect)
 
+def test_dependencies(module):
+    if not python_elasticsearch_installed:
+        module.fail_json(msg="python3-elasticsearch required for this module. "
+                            "see https://github.com/elastic/elasticsearch-py")
+
 def add_document(module):
-
     es_api = get_elasticsearch_connect(module)
-
     index = module.params.get('index')
     body = module.params.get('body')
-    body = json.dumps(body)
-
+    body = json.dumps(body) #convert to json
+    #id = module.params.get('id')
     if index is NOT_SET:
         raise AssertionError('Index not set `NOT_SET`')
     if body is NOT_SET:
         raise AssertionError('Body not set `NOT_SET`')
 
-    #raise AssertionError(body)
+    #raise AssertionError(id)
+
     es_api.indices.create(index = index, ignore = 400)
 
     if module.params.get('id') is NOT_SET:
@@ -113,18 +106,11 @@ def add_document(module):
       changed = es_api.index(index = index, id = id, body = body)
 
     module.exit_json(changed=changed,
-                     index=index,
-                     body=body)
-
-
-def test_dependencies(module):
-    if not python_elasticsearch_installed:
-        module.fail_json(msg="python3-elasticsearch required for this module. "
-                             "see https://github.com/elastic/elasticsearch-py")
+                    index=index,
+                    body=body)
 
 
 def main():
-
     module = AnsibleModule(
         argument_spec = dict(
             host   = dict(type = "str", default = "localhost"),
@@ -133,7 +119,7 @@ def main():
             id     = dict(type = "int"),
             index  = dict(type = "str", default = "xavki"),
             scheme = dict(type = "str", default = "http"),
-            state  = dict(type = "str", default = "present", choices=["acquire", "present", "absent"]),
+            state  = dict(type = "str", default = "present", choices = ["acquire", "present", "absent"])
         ),
         supports_check_mode=True
     )
@@ -147,7 +133,6 @@ def main():
             module.params.get('host'), module.params.get('port'), e))
     except Exception as e:
         module.fail_json(msg=str(e))
-
 
 if __name__ == '__main__':
     main()
